b683226ad3fce5a9f697fdea42773f71c93d7751
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-15 16:51:32 +0700

    [#002] init metro4

---
+++ admins/admin_login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Metro UI -->
+    <link rel="stylesheet" href="https://cdn.korzh.com/metroui/v4/css/metro-all.min.css">
+
+    <link rel="stylesheet" href="../resources/css/responsive_style.css">
+    <title>Admin Login</title>
+  </head>
+  <body>
+    <div class="container">
+        <h1>Hello, world!</h1>
+    </div>    
+
+    <!-- Metro UI -->
+    <script src="https://cdn.korzh.com/metroui/v4.5.1/js/metro.min.js"></script>
+  </body>
+</html>
