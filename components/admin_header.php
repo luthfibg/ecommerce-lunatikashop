497a30a9221efb75f1a3ec2a6b27175f2cf9be53
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-18 14:51:59 +0700

    [#017] admin header init

---
+++ components/admin_header.php
@@ -0,0 +1,22 @@
+<header class="header">
+    <section class="flex">
+        <a href="dashboard.php" class="logo">Admin
+            <span>Panel</span>
+        </a>
+        <nav class="navbar">
+            <a href="dashboard.php">Dashboard</a>
+            <a href="products.php">Products</a>
+            <a href="placed_orders.php">Placed Orders</a>
+            <a href="admin_accounts.php">Admin Accounts</a>
+            <a href="user_accounts.php">User Accounts</a>
+            <a href="messages.php">Messages</a>
+        </nav>
+        <div class="icons">
+            <div id="menu-btn" class="fas fa-bars"></div>
+            <div id="user-btn" class="fas fa-user"></div>
+        </div>
+        <div class="profile">
+            <?php ?>
+        </div>
+    </section>
+</header>
