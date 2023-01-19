ad41cf24664db655fb1311ee0b024f74e6ba4b86
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-20 00:10:24 +0700

    [#039] update profile untest

--- components/update_profile_card.php
+++ components/update_profile_card.php
@@ -7,19 +7,26 @@
     </div>
     <div class="row p-2 d-flex flex-column text-center">
         <form action="" method="POST">
-            <input type="hidden" name="old_pass" value="<?= $fetch_profile['password'] ?>">
+            <input type="hidden" name="previous_pass" value="<?= $fetch_profile['password'] ?>">
             <div class="mb-3">
                 <input type="text" name="name" oninput="this.value = this.value.replace(/\s/g, '')" data-role="input"
                     class="form-control" id="inputUsername" value="<?= $fetch_profile['name'] ?>" placeholder="
-                    Username">
+                    Username" required>
             </div>
             <div class="mb-5">
-                <input type="password" name="new_password" oninput="this.value = this.value.replace(/\s/g, '')"
-                    class="form-control" id="inputNewPassword1" placeholder="New Password">
+                <input type="password" name="old_password" maxlength="20"
+                    oninput="this.value = this.value.replace(/\s/g, '')" class="form-control" id="inputNewPassword1"
+                    placeholder="Old Password" required>
             </div>
             <div class="mb-5">
-                <input type="password" name="confirm_new_password" oninput="this.value = this.value.replace(/\s/g, '')"
-                    class="form-control" id="inputNewPassword2" placeholder="Confirm New Password">
+                <input type="password" name="new_password" maxlength="20"
+                    oninput="this.value = this.value.replace(/\s/g, '')" class="form-control" id="inputNewPassword1"
+                    placeholder="New Password" required>
+            </div>
+            <div class="mb-5">
+                <input type="password" name="confirm_new_password" maxlength="20"
+                    oninput="this.value = this.value.replace(/\s/g, '')" class="form-control" id="inputNewPassword2"
+                    placeholder="Confirm New Password" required>
             </div>
             <input type="submit" name="submit_login" class="button secondary component-tone mt-5" value="Update">
         </form>
--- update_profile.php
+++ update_profile.php
@@ -7,8 +7,42 @@ $footer = '';
 include('components/connection.php');
 include('layouts/update_profile_layout.php');
 
-$select_profile = $conn->prepare("SELECT * FROM `admins` WHERE id = ?");
-$select_profile->execute([$admin_id]);
-$fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+    $update_name = $conn->prepare("UPDATE `admins` SET name = ? WHERE id = ?");
+    $update_name->execute([$name, $admin_id]);
+
+    $empty_pass = 'da39a3ee5e6b4b0d3255bfef95601890afd80709';
+    $select_old_pass = $conn->prepare("SELECT password FROM `admins` WHERE id = ?");
+    $select_old_pass->execute([$admin_id]);
+    $fetch_previous_pass = $select_old_pass->fetch(PDO::FETCH_ASSOC);
+    $previous_pass = $fetch_previous_pass['password'];
+
+    $old_pass = $_POST['old_password'];
+    $old_pass = filter_var($old_pass, FILTER_SANITIZE_STRING);
+    $new_pass = $_POST['new_password'];
+    $new_pass = filter_var($new_pass, FILTER_SANITIZE_STRING);
+    $confirm_pass = $_POST['confirm_password'];
+    $confirm_pass = filter_var($confirm_pass, FILTER_SANITIZE_STRING);
+
+    if ($old_pass == $empty_pass) {
+        $message[] = 'Please enter old password';
+    } else if ($old_pass != $new_pass) {
+        $message[] = 'Old password didn\'t matched';
+    } else if ($new_pass != $confirm_pass) {
+        $message[] = 'Password didn\'t matched';
+    } else {
+        if ($new_pass != $empty_pass) {
+            $update_pass = $conn->prepare("UPDATE `admins` SET password = ? WHERE id = ?");
+            $update_pass->execute([$confirm_pass, $admin_id]);
+            $message[] = 'Your password changed';
+            header('location:dashboard.php');
+        } else {
+            $message[] = 'Enter new password please';
+        }
+    }
+}
 
 ?>
