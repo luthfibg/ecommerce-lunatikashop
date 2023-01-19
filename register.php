13537b4f4b8f8cd79fec0065d8472422c46715f0
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-19 16:42:30 +0700

    [#033] create some new page

--- register.php
+++ register.php
@@ -9,24 +9,29 @@ include('layouts/register_layout.php');
 
 session_start();
 
-// if (isset($_POST['submit_register'])) {
+if (isset($_POST['submit_register'])) {
 
-//     $name = $_POST['name'];
-//     $name = filter_var(htmlspecialchars($name));
-//     $pass = sha1($_POST['password']);
-//     $pass = filter_var(htmlspecialchars($pass));
+    $name = $_POST['name'];
+    $name = filter_var(htmlspecialchars($name));
+    $pass = sha1($_POST['password']);
+    $pass = filter_var(htmlspecialchars($pass));
+    $conf_pass = sha1($_POST['confirm_password']);
+    $conf_pass = filter_var(htmlspecialchars($conf_pass));
 
-//     $select_admin = $conn->prepare("SELECT * FROM `admins` WHERE name = ? AND password = ?");
-//     $select_admin->execute([$name, $pass]);
+    $select_admin = $conn->prepare("SELECT * FROM `admins` WHERE name = ?");
+    $select_admin->execute([$name]);
 
-//     if ($select_admin->rowCount() > 0) {
-//         $fetch_admin_id = $select_admin->fetch(PDO::FETCH_ASSOC);
-//         $_SESSION['admin_id'] = $fetch_admin_id['id'];
-//         $message[] = "you are logged in!";
-//         header('location:dashboard.php');
-//     } else {
-//         $message[] = "incorrect username or password!";
-//     }
-// }
+    if ($select_admin->rowCount() > 0) {
+        $message[] = 'User already exists';
+    } else {
+        if ($pass != $conf_pass) {
+            $message[] = 'Password doesn\'t matched!';
+        } else {
+            $insert_admin = $conn->prepare("INSERT INTO `admins` (name, password) VALUES (?,?)");
+            $insert_admin->execute([$name, $conf_pass]);
+            $message[] = 'New admin registered';
+        }
+    }
+}
 
 ?>
---
+++ update_profile.php
@@ -0,0 +1,32 @@
+<?php
+
+$title = 'Lunatika Shop';
+$header = '';
+$content = 'components/update_profile_card.php';
+$footer = '';
+include('components/connection.php');
+include('layouts/update_profile_layout.php');
+
+session_start();
+
+// if (isset($_POST['submit_register'])) {
+
+//     $name = $_POST['name'];
+//     $name = filter_var(htmlspecialchars($name));
+//     $pass = sha1($_POST['password']);
+//     $pass = filter_var(htmlspecialchars($pass));
+
+//     $select_admin = $conn->prepare("SELECT * FROM `admins` WHERE name = ? AND password = ?");
+//     $select_admin->execute([$name, $pass]);
+
+//     if ($select_admin->rowCount() > 0) {
+//         $fetch_admin_id = $select_admin->fetch(PDO::FETCH_ASSOC);
+//         $_SESSION['admin_id'] = $fetch_admin_id['id'];
+//         $message[] = "you are logged in!";
+//         header('location:dashboard.php');
+//     } else {
+//         $message[] = "incorrect username or password!";
+//     }
+// }
+
+?>
