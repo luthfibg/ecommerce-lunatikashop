7c74fdf295f281e8b6908f64c5fd62caf5b38c88
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-22 21:23:59 +0700

    [#069] init update product file

---
+++ update_product.php
@@ -0,0 +1,15 @@
+<?php
+include('components/connection/.php');
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+if (!isset($admin_id)) {
+    header('location:login.php');
+}
+
+$title = "Product Management";
+
+include('layouts/update_product_layout.php');
+
+?>
