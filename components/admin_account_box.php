b00e0489a7b5f9a985a5e6375ea6f12707f6b2a4
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-24 15:08:45 +0700

    [#099] beautifying admin account card

--- components/admin_account_box.php
+++ components/admin_account_box.php
@@ -1,8 +1,10 @@
 <div class="box mb-3 g-col-12 g-col-md-6">
-    <span class="d-block">Admin id:&nbsp;
+    <span class="d-block">
+        <span class="keyspan">Admin id:&nbsp;</span>
         <?= $fetch_admin_accounts['id'] ?>
     </span>
-    <span class="d-block">Admin name:&nbsp;
+    <span class="d-block">
+        <span class="keyspan">Admin name:&nbsp;</span>
         <?= $fetch_admin_accounts['name'] ?>
     </span>
     <div class="flex-action d-flex mt-5">
