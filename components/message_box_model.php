657a32dffd71dc00111beb65ab4881188016ee98
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-27 16:58:52 +0700

    [#107] setup messages page

--- components/message_box_model.php
+++ components/message_box_model.php
@@ -1,3 +1,26 @@
-<div class="box">
-    <span>Message Coming Soon...</span>
+<div class="box g-col-12 g-col-md-6">
+    <span class="d-block">
+        <span class="keyspan">User ID:&nbsp;</span>
+        <?= $fetch_messages['user_id'] ?>
+    </span>
+    <span class="d-block">
+        <span class="keyspan">User Name:&nbsp;</span>
+        <?= $fetch_messages['name'] ?>
+    </span>
+    <span class="d-block">
+        <span class="keyspan">User Phone:&nbsp;</span>
+        <?= $fetch_messages['number'] ?>
+    </span>
+    <span class="d-block">
+        <span class="keyspan">User Email:&nbsp;</span>
+        <?= $fetch_messages['email'] ?>
+    </span>
+    <span class="d-block">
+        <span class="keyspan">User Message:&nbsp;</span>
+        <?= $fetch_messages['message'] ?>
+    </span>
+    <div class="flex-action d-flex mt-5">
+        <a href="messages.php?delete=<?= $fetch_messages['id']; ?>" class="btn btn-sm btn-delete"
+            onclick="return confirm('Are you sure to delete this message?'); ">Delete</a>
+    </div>
 </div>
