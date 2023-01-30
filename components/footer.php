924372a53eb6844c841301729da94b9cc5428db9
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-30 16:26:36 +0700

    [#120] footer build breakpoint

--- components/footer.php
+++ components/footer.php
@@ -1,16 +1,29 @@
 <footer class="footer">
     <section class="box-container">
         <div class="box">
-            <a href="#"><i class="fas fa-angle-right"></i></a>
-            <a href="#"><i class="fas fa-angle-right"></i></a>
-            <a href="#"><i class="fas fa-angle-right"></i></a>
-            <a href="#"><i class="fas fa-angle-right"></i></a>
+            <h4>Quick Links</h4>
+            <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="shop.php"><i class="fas fa-angle-right"></i>shop</a>
+            <a href="contact.php"><i class="fas fa-angle-right"></i>contact</a>
         </div>
         <div class="box">
-            <a href="#"><i class="fas fa-angle-right"></i></a>
-            <a href="#"><i class="fas fa-angle-right"></i></a>
-            <a href="#"><i class="fas fa-angle-right"></i></a>
-            <a href="#"><i class="fas fa-angle-right"></i></a>
+            <h4>Extra Links</h4>
+            <a href="orders.php"><i class="fas fa-angle-right"></i>my orders</a>
+            <a href="cart.php"><i class="fas fa-angle-right"></i>cart</a>
+            <a href="wishlist.php"><i class="fas fa-angle-right"></i>wishlist</a>
+            <a href="logout.php"><i class="fas fa-angle-right"></i>logout</a>
+        </div>
+        <div class="box">
+            <h4>Contact Info</h4>
+            <a href="tel:+62822217777">
+                <i class="fas fa-phone"></i>
+                +628-2221-7777
+            </a>
+            <a href="mailto:info@lunatika.co.id">
+                <i class="fas fa-envelope"></i>
+                info@lunatika.co.id
+            </a>
         </div>
     </section>
 </footer>
