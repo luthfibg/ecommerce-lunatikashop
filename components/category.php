7e6cbfaa375b8253faa625b8c9afe505d53a7fc3
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-05 15:36:08 +0700

    [#135] home category breakpoint

--- components/category.php
+++ components/category.php
@@ -1,5 +1,8 @@
 <div class="category-slider">
     <div class="w">
-        <a href="#" class="slide"></a>
+        <a href="#" class="slide">
+            <img src="" alt="image category 1">
+            <h4>Amazfit</h4>
+        </a>
     </div>
 </div>
