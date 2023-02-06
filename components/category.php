76c2f99935c732dc2c925d8b55c55b0d6ea48fa5
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-06 10:19:15 +0700

    [#136] home category breakpoint 2

--- components/category.php
+++ components/category.php
@@ -1,8 +1,28 @@
-<div class="category-slider">
-    <div class="w">
-        <a href="#" class="slide">
-            <img src="" alt="image category 1">
-            <h4>Amazfit</h4>
+<div class="category-slider py-3 py-5-md px-5">
+    <div class="w d-flex justify-content-between flex-wrap">
+        <a href="category.php?category=gel" class="slide">
+            <img src="assets/images/icons/gadgets.png" alt="image category 1">
+            <h4>Gadget</h4>
+        </a>
+        <a href="category.php?category=fur" class="slide">
+            <img src="assets/images/icons/furniture.png" alt="image category 1">
+            <h4>Furniture</h4>
+        </a>
+        <a href="category.php?category=cth" class="slide">
+            <img src="assets/images/icons/clothes-hanger.png" alt="image category 1">
+            <h4>Clothes</h4>
+        </a>
+        <a href="category.php?category=gro" class="slide">
+            <img src="assets/images/icons/shopping-bag.png" alt="image category 1">
+            <h4>Grocery</h4>
+        </a>
+        <a href="category.php?category=tls" class="slide">
+            <img src="assets/images/icons/settings.png" alt="image category 1">
+            <h4>Tools</h4>
+        </a>
+        <a href="category.php?category=svc" class="slide">
+            <img src="assets/images/icons/customer-service.png" alt="image category 1">
+            <h4>Services</h4>
         </a>
     </div>
 </div>
