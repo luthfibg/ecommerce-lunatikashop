43703a1cda895c01695ff09cf0d1b587cc8444ce
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-06 15:16:12 +0700

    [#140] switch flex to swiper.js for category section fixed

--- resources/js/swiper-keyboard.js
+++ resources/js/swiper-keyboard.js
@@ -15,3 +15,32 @@ var swiper = new Swiper('.home-carousel', {
   grabCursor: true,
   loop: true,
 });
+
+var swiper = new Swiper('.category-slider', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  },
+  grabCursor: true,
+  loop: true,
+});
