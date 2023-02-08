782d5ee3306e9051dd8c702e8695d46d8fd06bcf
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-08 16:43:10 +0700

    [#157] products home section 4 init

--- resources/js/swiper-keyboard.js
+++ resources/js/swiper-keyboard.js
@@ -119,3 +119,28 @@ var swiper3 = new Swiper('.products-cloth-slider', {
     },
   },
 });
+
+var swiper3 = new Swiper('.products-grocery-slider', {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
