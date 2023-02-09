f75090c7f975c556783a92360b8152db2b399cfe
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-09 12:43:30 +0700

    [#165] wishlist data flow setup

--- wishlist.php
+++ wishlist.php
@@ -15,4 +15,36 @@ if (!isset($user_id)) {
 
 include('layouts/wishlist_layout.php');
 
+if (isset($_POST['add_to_wishlist'])) {
+    if ($user_id == '') {
+        header('location:user_login.php');
+    } else {
+
+        $pid = $_POST['pid'];
+        $pid = filter_var(htmlspecialchars($pid));
+        $name = $_POST['name'];
+        $name = filter_var(htmlspecialchars($name));
+        $price = $_POST['price'];
+        $price = filter_var(htmlspecialchars($price));
+        $image = $_POST['image'];
+        $image = filter_var(htmlspecialchars($image));
+
+        $check_wishlist_nums = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+        $check_wishlist_nums->execute([$user_id, $name]);
+
+        $check_cart_nums = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+        $check_cart_nums->execute([$user_id, $name]);
+
+        if ($check_wishlist_nums->rowCount() > 0) {
+            $message[] = 'Wishlist already exist !';
+        } elseif ($check_cart_nums->rowCount() > 0) {
+            $message[] = 'Cart already exist';
+        } else {
+            $insert_wishlist = $conn->prepare("INSERT INTO `wishlist` (user_id, pid, name, price, image) VALUES (?, ?, ?, ?, ?)");
+            $insert_wishlist->execute([$user_id, $pid, $name, $price, $image]);
+            $message[] = 'SUccess add wishlist!';
+        }
+    }
+}
+
 ?>
