8b14de30a012af142b7e6e87476adff2fc66c068
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-09 13:47:44 +0700

    [#166] cart & wishlist done with deffered by user authentication

--- wishlist.php
+++ wishlist.php
@@ -28,21 +28,26 @@ if (isset($_POST['add_to_wishlist'])) {
         $price = filter_var(htmlspecialchars($price));
         $image = $_POST['image'];
         $image = filter_var(htmlspecialchars($image));
-
-        $check_wishlist_nums = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
-        $check_wishlist_nums->execute([$user_id, $name]);
+        $qty = $_POST['qty'];
+        $qty = filter_var(htmlspecialchars($qty));
 
         $check_cart_nums = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
         $check_cart_nums->execute([$user_id, $name]);
 
-        if ($check_wishlist_nums->rowCount() > 0) {
-            $message[] = 'Wishlist already exist !';
-        } elseif ($check_cart_nums->rowCount() > 0) {
+        if ($check_cart_nums->rowCount() > 0) {
             $message[] = 'Cart already exist';
         } else {
-            $insert_wishlist = $conn->prepare("INSERT INTO `wishlist` (user_id, pid, name, price, image) VALUES (?, ?, ?, ?, ?)");
-            $insert_wishlist->execute([$user_id, $pid, $name, $price, $image]);
-            $message[] = 'SUccess add wishlist!';
+            $check_wishlist_nums = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+            $check_wishlist_nums->execute([$name, $user_id]);
+
+            if ($check_wishlist_nums->rowCount() > 0) {
+                $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
+                $delete_wishlist->execute([$name, $user_id]);
+            }
+
+            $insert_cart = $conn->prepare("INSERT INTO `cart` (user_id, pid, name, price, quantity, image) VALUES (?, ?, ?, ?, ?, ?)");
+            $insert_cart->execute([$user_id, $pid, $name, $price, $qty, $image]);
+            $message[] = 'Success add cart!';
         }
     }
 }
