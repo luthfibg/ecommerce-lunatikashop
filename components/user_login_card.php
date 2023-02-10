291b6d37de8adcef39001e9a3f1f4aebc59db188
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-10 09:56:52 +0700

    [#168] user login breakpoint

--- components/user_login_card.php
+++ components/user_login_card.php
@@ -5,14 +5,14 @@
     <div class="row p-2 d-flex flex-column text-center">
         <form action="" method="POST">
             <div class="mb-3">
-                <input type="text" name="name" oninput="this.value = this.value.replace(/\s/g, '')" data-role="input"
-                    class="form-control" id="inputUsername" placeholder="Username">
+                <input type="text" name="username" oninput="this.value = this.value.replace(/\s/g, '')"
+                    data-role="input" class="form-control" id="inputUsername" placeholder="Username">
             </div>
             <div class="mb-5">
                 <input type="password" name="password" oninput="this.value = this.value.replace(/\s/g, '')"
                     class="form-control" id="exampleInputPassword1" placeholder="Password">
             </div>
-            <input type="submit" name="submit_login" class="button secondary component-tone mt-5" value="Login">
+            <input type="submit" name="user_submit_login" class="button secondary component-tone mt-5" value="Login">
         </form>
         <span>Haven't account? <a href="register.php">register</a></span>
     </div>
--- user_login.php
+++ user_login.php
@@ -15,6 +15,14 @@ if (!isset($_SESSION[$user_id])) {
     $user_id = '';
 }
 
+if (isset($_POST['user_submit_login'])) {
+
+    $username = $_POST['username'];
+    $username = filter_var(htmlspecialchars($username));
+    $password = $_POST['password'];
+    $password = filter_var(htmlspecialchars($password));
+}
+
 include('layouts/user_login_layout.php');
 
 ?>
