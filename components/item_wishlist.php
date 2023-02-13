fe477dfb53b0e9f16ab99cdd297b1080654b768b
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-13 16:18:37 +0700

    [#176] fix badge icon list cart&wishlist

--- components/item_wishlist.php
+++ components/item_wishlist.php
@@ -4,20 +4,20 @@
             <div class="fw-bold">Subheading</div>
             Content for list item
         </div>
-        <span class="badge bg-primary rounded-pill">14</span>
+        <span class="badge bg-primary rounded-pill mt-4 me-4">14</span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-start bg-dark">
         <div class="ms-2 me-auto">
             <div class="fw-bold">Subheading</div>
             Content for list item
         </div>
-        <span class="badge bg-primary rounded-pill">14</span>
+        <span class="badge bg-primary rounded-pill mt-4 me-4">14</span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-start bg-dark">
         <div class="ms-2 me-auto">
             <div class="fw-bold">Subheading</div>
             Content for list item
         </div>
-        <span class="badge bg-primary rounded-pill">14</span>
+        <span class="badge bg-primary rounded-pill mt-4 me-4">14</span>
     </li>
 </ol>
