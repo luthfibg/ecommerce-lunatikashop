6988c7e6ebee00a87483b060f9c66093c5f42eb8
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-15 14:55:15 +0700

    [#183] show wishlist & cart items

--- components/item_cart.php
+++ components/item_cart.php
@@ -1,23 +1,37 @@
-<ol class="list-group list-group-numbered mb-3">
-    <li class="list-group-item d-flex flex-wrap justify-content-between align-items-center bg-dark">
-        <div class="ms-2 me-auto">
-            <div class="fw-bold">Subheading</div>
-            Content for list item
-        </div>
-        <span class="badge bg-primary rounded-pill mt-4 me-4">14</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-start bg-dark">
-        <div class="ms-2 me-auto">
-            <div class="fw-bold">Subheading</div>
-            Content for list item
-        </div>
-        <span class="badge bg-primary rounded-pill mt-4 me-4">14</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-start bg-dark">
-        <div class="ms-2 me-auto">
-            <div class="fw-bold">Subheading</div>
-            Content for list item
-        </div>
-        <span class="badge bg-primary rounded-pill mt-4 me-4">14</span>
-    </li>
+<ol class="list-group list-group-numbered bg-dark mb-3">
+
+    <?php
+
+    $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+    $select_cart->execute([$user_id]);
+
+    if ($select_cart->rowCount() > 0) {
+        while ($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)) {
+            $price = $fetch_cart['price'];
+            $qty = $fetch_cart['quantity'];
+            $fixed_price = $price * $qty;
+            ?>
+            <li class="list-group-item d-flex justify-content-between align-items-start bg-dark py-3">
+                <div class="ms-2 me-auto">
+                    <div class="img-container">
+                        <img src="assets/images/products/<?= $fetch_cart['image']; ?>" alt="pid=<?= $fetch_cart['pid']; ?>"
+                            class="image mb-3 mb-5-md" style="width: 4rem;height: 4rem;">
+                    </div>
+                    <div class="fw-bold">
+                        <?= $fetch_cart['name']; ?>
+                    </div>
+                    Rp
+                    <?= currency_formatter($fixed_price); ?>
+                    ,-
+                </div>
+                <span class="badge bg-primary rounded-pill mt-4 me-4">
+                    <?= $fetch_cart['quantity']; ?>
+                </span>
+            </li>
+            <?php
+        }
+    }
+
+    ?>
+
 </ol>
--- components/item_wishlist.php
+++ components/item_wishlist.php
@@ -1,23 +1,32 @@
 <ol class="list-group list-group-numbered bg-dark mb-3">
-    <li class="list-group-item d-flex justify-content-between align-items-start bg-dark">
-        <div class="ms-2 me-auto">
-            <div class="fw-bold">Subheading</div>
-            Content for list item
-        </div>
-        <span class="badge bg-primary rounded-pill mt-4 me-4">14</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-start bg-dark">
-        <div class="ms-2 me-auto">
-            <div class="fw-bold">Subheading</div>
-            Content for list item
-        </div>
-        <span class="badge bg-primary rounded-pill mt-4 me-4">14</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-start bg-dark">
-        <div class="ms-2 me-auto">
-            <div class="fw-bold">Subheading</div>
-            Content for list item
-        </div>
-        <span class="badge bg-primary rounded-pill mt-4 me-4">14</span>
-    </li>
+
+    <?php
+
+    $select_wishlist = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id = ?");
+    $select_wishlist->execute([$user_id]);
+
+    if ($select_wishlist->rowCount() > 0) {
+        while ($fetch_wishlist = $select_wishlist->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+            <li class="list-group-item d-flex justify-content-between align-items-start bg-dark py-3">
+                <div class="ms-2 me-auto">
+                    <div class="img-container">
+                        <img src="assets/images/products/<?= $fetch_wishlist['image']; ?>"
+                            alt="pid=<?= $fetch_wishlist['pid']; ?>" class="image mb-3 mb-5-md"
+                            style="width: 4rem;height: 4rem;">
+                    </div>
+                    <div class="fw-bold">
+                        <?= $fetch_wishlist['name']; ?>
+                    </div>Rp
+                    <?= currency_formatter($fetch_wishlist['price']); ?>
+                    ,-
+                </div>
+                <span class="badge bg-primary rounded-pill mt-4 me-4">new</span>
+            </li>
+            <?php
+        }
+    }
+
+    ?>
+
 </ol>
