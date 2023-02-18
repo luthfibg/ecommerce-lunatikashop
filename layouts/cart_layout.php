ae0255db2af594c0f3c4e517e3eeb4f2458689c8
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-18 08:33:59 +0700

    [#206] insert new products (admin test) 1

--- layouts/cart_layout.php
+++ layouts/cart_layout.php
@@ -1,14 +1,3 @@
-<?php
-
-// include('components/connection.php');
-
-// session_start();
-
-// $admin_id = $_SESSION['admin_id'];
-// if (!isset($admin_id)) {
-//     header('location:login.php');
-// }
-?>
 <!DOCTYPE html>
 <html lang="en">
 
