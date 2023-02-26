3cbc8724b3be5bcf4cc9716040895d8cf06f8d71
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-27 05:18:03 +0700

    [#223] review add assets

--- components/reviews.php
+++ components/reviews.php
@@ -3,7 +3,46 @@
     <div class="reviews-slider">
         <div class="w">
             <div class="slider">
-                <img src="assets/images/avatars/john.png" alt="user1" style="width: 6rem;">
+                <img src="assets/images/avatars/yoga_yobek.jpg" alt="user1" style="width: 6rem;">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, labore? Voluptates laboriosam illo
+                    nihil amet blanditiis delectus consequuntur?</p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-half-alt"></i>
+                </div>
+                <h4>Yoga Handinata</h4>
+            </div>
+            <div class="slider">
+                <img src="assets/images/avatars/fakhri_labib.jpg" alt="user2" style="width: 6rem;">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, labore? Voluptates laboriosam illo
+                    nihil amet blanditiis delectus consequuntur?</p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-half-alt"></i>
+                </div>
+                <h4>Fakhri Labib</h4>
+            </div>
+            <div class="slider">
+                <img src="assets/images/avatars/yoel_peterson.jpg" alt="user3" style="width: 6rem;">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, labore? Voluptates laboriosam illo
+                    nihil amet blanditiis delectus consequuntur?</p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-half-alt"></i>
+                </div>
+                <h4>Yoel Peterson</h4>
+            </div>
+            <div class="slider">
+                <img src="assets/images/avatars/john.png" alt="user4" style="width: 6rem;">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, labore? Voluptates laboriosam illo
                     nihil amet blanditiis delectus consequuntur?</p>
                 <div class="stars">
@@ -14,6 +53,45 @@
                     <i class="fa-solid fa-half-alt"></i>
                 </div>
                 <h4>John Cena</h4>
+            </div>
+            <div class="slider">
+                <img src="assets/images/avatars/hendriyan_david.jpg" alt="user5" style="width: 6rem;">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, labore? Voluptates laboriosam illo
+                    nihil amet blanditiis delectus consequuntur?</p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-half-alt"></i>
+                </div>
+                <h4>Hendriyan David</h4>
+            </div>
+            <div class="slider">
+                <img src="assets/images/avatars/irma_kumala_dewi.jpg" alt="user6" style="width: 6rem;">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, labore? Voluptates laboriosam illo
+                    nihil amet blanditiis delectus consequuntur?</p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-half-alt"></i>
+                </div>
+                <h4>Irma Kumala Dewi</h4>
+            </div>
+            <div class="slider">
+                <img src="assets/images/avatars/nova_risma.jpg" alt="user1" style="width: 6rem;">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, labore? Voluptates laboriosam illo
+                    nihil amet blanditiis delectus consequuntur?</p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-half-alt"></i>
+                </div>
+                <h4>Nova Risma</h4>
             </div>
         </div>
     </div>
