dd2e71577931ceb6726afb3dcab572a92e62e7fb
luthfibg <luthfinewzoneonline@gmail.com>
2023-03-02 17:05:34 +0700

    [#237] custom contact input 2

--- components/contact_form.php
+++ components/contact_form.php
@@ -1,7 +1,8 @@
-<form action="" method="post" class="card">
+<form action="" method="post" class="card p-3">
     <input type="text" name="name" id="name" class="form-control" placeholder="Name" maxlength="55" required>
     <input type="tel" name="phone" id="phone" class="form-control" placeholder="Phone Number" required>
     <input type="email" name="email" id="email" class="form-control" placeholder="Email Address" maxlength="55"
         required>
+    <textarea class="form-control" name="msg" id="message" cols="30" rows="6" placeholder="Type Message"></textarea>
 </form>
 <div class="alert alert-info" style="display: none;"></div>
