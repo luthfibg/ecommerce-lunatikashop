f91e3438d0684aede1de1a0a9463b5b819268aca
luthfibg <luthfinewzoneonline@gmail.com>
2023-03-03 16:44:57 +0700

    [#248] fit result search design for mobile

--- components/search_product_result.php
+++ components/search_product_result.php
@@ -1,11 +1,11 @@
 <div class="row search-product-result py-3 py-5-md">
-    <div class="products-row-header mb-3 mb-5-md">Result</div>
+    <div class="products-result-header mb-3 mb-5-md">Result</div>
     <div class="wrapper d-flex align-items-center flex-wrap">
         <?php
 
         if (isset($_POST['search_box']) or isset($_POST['search_btn'])) {
             $search_box = $_POST['search_box'];
-            $select_products = $conn->prepare("SELECT * FROM `products` WHERE name LIKE '%{$search_box}%'");
+            $select_products = $conn->prepare("SELECT * FROM `products` WHERE name LIKE '%{$search_box}%' OR category LIKE '%{$search_box}%' OR price LIKE '%{$search_box}%'");
             $select_products->execute();
 
             if ($select_products->rowCount() > 0) {
@@ -17,7 +17,7 @@
                 }
             }
         } else {
-            echo '<p class="empty">Nothing product to display</p>';
+            echo '<p class="empty" style="font-size: 1.65rem;">Nothing product to display</p>';
         }
 
         ?>
