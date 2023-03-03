3f7f593debed3f266c303688423e5dda270fcaf3
luthfibg <luthfinewzoneonline@gmail.com>
2023-03-03 09:36:04 +0700

    [#243] user message sending logic

--- components/contact_form.php
+++ components/contact_form.php
@@ -1,10 +1,13 @@
 <form action="" method="post" class="card p-3">
+    <?php
+        $user_id;
+    ?>
     <input type="text" name="name" id="name" class="form-control" placeholder="Name" maxlength="55" required>
     <input type="tel" name="phone" id="phone" class="form-control" placeholder="Phone Number" required>
     <input type="email" name="email" id="email" class="form-control" placeholder="Email Address" maxlength="55"
         required>
     <textarea class="form-control" name="msg" id="message" cols="30" rows="6" placeholder="Type Message"></textarea>
-    <button type="submit" name="name" id="name" class="btn-custom btn-sm form-control mt-5 py-2">
+    <button type="submit" name="send_msg" id="send_msg" class="btn-custom btn-sm form-control mt-5 py-2">
         <i class="fa-regular fa-paper-plane"></i>&nbsp;
         Kirim
     </button>
--- contact.php
+++ contact.php
@@ -13,6 +13,30 @@ if (!isset($user_id)) {
     header('location:user_login.php');
 }
 
+if (isset($_POST['send_msg'])) {
+    $name = $_POST['name'];
+    $name = filter_var(htmlspecialchars($name));
+    $email = $_POST['email'];
+    $email = filter_var(htmlspecialchars($email));
+    $phone = $_POST['phone'];
+    $phone = filter_var(htmlspecialchars($phone));
+    $msg = $_POST['msg'];
+    $msg = filter_var(htmlspecialchars($msg));
+
+    $select_message = $conn->prepare("SELECT * FROM `messages` WHERE name = ? AND email = ? AND number = ? AND message = ?");
+    $select_message->execute([$name, $email, $phone, $msg]);
+
+    if ($select_message->rowCount() > 0) {
+        $message[] = 'Message already sent';
+    } else {
+        $send_message = $conn->prepare("INSERT INTO `messages` (name, email, number, message) VALUES (?, ?, ?, ?)");
+        $send_message->execute([$name, $email, $phone, $msg]);
+
+        $message[] = 'Message successfully sent!';
+    }
+
+}
+
 include('layouts/contact_layout.php');
 
 ?>
