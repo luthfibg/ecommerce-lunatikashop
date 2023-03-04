e5eb8c1b841148eef7a075511b452d7d1406c8fe
luthfibg <luthfinewzoneonline@gmail.com>
2023-03-04 08:32:19 +0700

    [#252] fixing wishlist uncompatibility

--- components/item_wishlist.php
+++ components/item_wishlist.php
@@ -1,12 +1,16 @@
-<ol class="list-group list-group-numbered bg-dark mb-3">
+<?php
 
-    <?php
+$select_wishlist = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id = ?");
+$select_wishlist->execute([$user_id]);
+$total_price = 0;
 
-    $select_wishlist = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id = ?");
-    $select_wishlist->execute([$user_id]);
-
-    if ($select_wishlist->rowCount() > 0) {
+if ($select_wishlist->rowCount() > 0) {
+    ?>
+    <ol class="list-group list-group-numbered bg-dark mb-3">
+        <?php
         while ($fetch_wishlist = $select_wishlist->fetch(PDO::FETCH_ASSOC)) {
+            $price = $fetch_wishlist['price'];
+            $total_price += $price;
             ?>
             <li class="list-group-item d-flex justify-content-between align-items-start bg-dark py-3">
                 <div class="d-flex w-100 align-items-center">
@@ -34,15 +38,50 @@
             </li>
             <?php
         }
-    } else {
         ?>
-        <div class="container d-flex justify-content-between flex-column align-items-center">
-            <span class="my-5">Wishlist is empty</span>
-            <div class="btn mb-5 w-100 w-50-md w-25-lg">Go To Shop</div>
-        </div>
-        <?php
-    }
-
+    </ol>
+    <ol class="list-group bottom-list bg-dark mb-5">
+        <li class="list-group-item bottom-list d-flex justify-content-between align-items-start bg-dark py-1 py-md-3">
+            <div class="d-flex w-100 align-items-center">
+                <div class="img-container d-flex justify-content-center align-items-center"
+                    style="width: 5rem;height: 5rem">
+                    <i class="fa-solid fa-shopping-cart fa-xl"></i>
+                </div>
+                <div class="me-auto ms-3 ms-md-5">
+                    <div class="fw-bold">
+                        Total Harga
+                    </div>
+                </div>
+                <div class="me-auto ms-3">
+                    Rp
+                    <?= currency_formatter($total_price); ?>
+                    ,-
+                </div>
+                <div class="action-remove me-1 me-md-3">
+                    <a href="payment.php" class="btn-custom remove px-3 py-2" style="color: var(--component-crimson);">
+                        <i class="fa-solid fa-circle-xmark fa-xl" style="color: var(--component-crimson);"></i>
+                        &nbsp; <span class="btn-value">Remove</span>
+                    </a>
+                </div>
+                <div class="action-checkout me-1 me-md-3">
+                    <a href="payment.php" class="btn-custom picker px-3 py-2" style="color: var(--light);">
+                        <i class="fa-solid fa-cart-shopping fa-xl" style="color: var(--component-emerald);"></i>
+                        &nbsp;<span class="btn-value">Upcart</span>
+                    </a>
+                </div>
+            </div>
+        </li>
+    </ol>
+    <?php
+} else {
     ?>
+    <div class="container d-flex justify-content-between flex-column align-items-center">
+        <span class="my-5">Wishlist is empty</span>
+        <div class="btn mb-5 w-100 w-50-md w-25-lg">Go To Shop</div>
+    </div>
+    <?php
+}
+
+?>
 
 </ol>
