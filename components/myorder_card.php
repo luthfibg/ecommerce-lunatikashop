f58e8edf1fc0bca17ade750db6a1a39f49e618e0
luthfibg <luthfinewzoneonline@gmail.com>
2023-03-08 05:12:45 +0700

    [#268] init card order breakpoint

--- components/myorder_card.php
+++ components/myorder_card.php
@@ -1,5 +1,10 @@
 <div class="card">
-    <h5 class="card-header">Featured</h5>
+    <div class="card-header">
+        <h5 class="title-card">
+            <?= $fetch_orders['placed_on']; ?>
+        </h5>
+        <h5 class="subtitle-card"></h5>
+    </div>
     <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
